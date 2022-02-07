114a500a8e1f2c46a3e649b79386b359503ea6e7
jpangia <jvpangia@gmail.com>
2022-02-07 12:47:28 -0500

    created stub stylist controller

---
+++ HairSalonBackEnd/HairSalonBackEnd/Controllers/StylistController.cs
@@ -0,0 +1,52 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HairSalonBackEnd.Controllers
+{
+    public class StylistController
+    {
+    }
+}
+*/
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HairSalonBackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StylistController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public StylistController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Stylist> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Stylist
+            {
+                
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Stylist stylist)
+        {
+            Console.WriteLine(stylist);
+        }
+    }
+}
