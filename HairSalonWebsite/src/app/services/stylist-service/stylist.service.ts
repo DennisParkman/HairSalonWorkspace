7b615ddf80ee9937fb02a003002d594b1344bd52
zeenal <zeenal.prajapati@bobcats.gcsu.edu>
2022-02-07 16:41:22 -0500

    addStylist() method need to be reviewed

--- HairSalonWebsite/src/app/services/stylist-service/stylist.service.ts
+++ HairSalonWebsite/src/app/services/stylist-service/stylist.service.ts
@@ -3,13 +3,22 @@ import { HttpClient } from '@angular/common/http'
 import { Stylist } from "../../models/stylist.model";
 import { Observable } from "rxjs";
 
-@Injectable({
+@Injectable(
+{
     providedIn: 'root'
 })
-export class StylistService {
+export class StylistService 
+{
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) 
+    { }
 
     readonly baseURL = 'http://localhost:63235/';
 
+    addStylist(stylist: Stylist): void
+    {
+        let url = this.baseURL.concat("Stylist");
+        console.log("Post");
+        this.http.post(url, stylist).subscribe();
+    }
 }
