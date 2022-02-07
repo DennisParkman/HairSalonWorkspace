e15f033f469ad3d60a2e9dfd8bc4d199e380bad2
Dennis <dennisparkman12@gmail.com>
2022-02-07 12:48:07 -0500

    Add models

---
+++ HairSalonWebsite/src/app/models/stylist.model.ts
@@ -0,0 +1,15 @@
+export class Stylist 
+{
+    name!: string;
+    id!: number;
+    level!: number;
+    bio!: string;
+
+    Stylist(name: string, id: number, level: number, bio: string) {
+        this.name = name;
+        this.id = id;
+        this.level = level;
+        this.bio = bio;
+    }
+
+}
