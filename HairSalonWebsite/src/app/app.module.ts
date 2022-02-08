006fc714920ad8ea1f5817679c268bc1f4eda4a6
jpangia <jvpangia@gmail.com>
2022-02-08 11:34:49 -0500

    Midway push for navbar. Note that I added a bootstrap reference to index.html, commented out the app-home-page reference in app.component.html
    Looks like it's ready for review, actually.

--- HairSalonWebsite/src/app/app.module.ts
+++ HairSalonWebsite/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { WeatherService } from './services/weather.service';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule(
 {
   declarations: 
   [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    NavbarComponent
   ],
   imports: 
   [
