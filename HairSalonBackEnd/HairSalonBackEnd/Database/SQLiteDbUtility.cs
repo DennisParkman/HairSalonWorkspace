489f2b584f49d6fd3cf1bb44a20f8f0e6da5a9fb
Dennis <dennis.parkman@bobcats.gcsu.edu>
2022-02-09 00:29:59 -0500

    Added fixed stylist table

---
+++ HairSalonBackEnd/HairSalonBackEnd/Database/SQLiteDbUtility.cs
@@ -0,0 +1,65 @@
+﻿using HairSalonBackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace HairSalonBackEnd.Database
+{
+    public static class SQLiteDbUtility
+    {
+        private static SQLiteDbContext dbContext;
+
+        private static bool isInitialized = false;
+
+        public static void InitializeDB()
+        {
+            if(!isInitialized)
+            {
+                dbContext = new SQLiteDbContext();
+                dbContext.Database.EnsureCreated();
+
+                isInitialized = true;
+            }
+        }
+
+        #region Stylist Methods
+
+        public static void AddStylist(Stylist stylist)
+        {
+            dbContext.Stylists.Add(stylist);
+            dbContext.SaveChanges();
+        }
+
+        public static IEnumerable<Stylist> GetAllStylists()
+        {
+            return dbContext.Stylists.ToList();
+        }
+
+        #endregion
+
+        private class SQLiteDbContext : DbContext
+        {
+            public DbSet<Stylist> Stylists { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite("FileName=Database/sqlite.db", option =>
+                {
+                    option.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+                });
+                base.OnConfiguring(optionsBuilder);
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Stylist>().ToTable("Stylists", "localSchema");
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+    }
+
+    
+}
