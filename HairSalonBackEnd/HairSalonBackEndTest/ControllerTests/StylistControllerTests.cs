57770ec3f00323564c89a68069db0912a898bd6d
jpangia <jvpangia@gmail.com>
2022-02-25 18:44:15 -0500

    needed to use a different unit test project; still no working unit test methods

---
+++ HairSalonBackEnd/HairSalonBackEndTest/ControllerTests/StylistControllerTests.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using HairSalonBackEnd.Controllers;
+using Microsoft.Extensions.Logging;
+using HairSalonBackEnd.Models;
+
+namespace HairSalonBackEndTest
+{
+    [TestClass]
+    public class StylistControllerTests
+    {
+
+        //or should I just add, view, update, view, and delete a stylist all in one test method, actually updating the database?
+        /// <summary>
+        /// tests StylistController.Post(stylist: Stylist) to see that it properly sends a stylist object
+        /// </summary>
+        [TestMethod]
+        public void TestPost()
+        {
+            //it should be fine; we never use the logger. Right?
+            ILogger<StylistController> test_logger =
+                LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger<StylistController>();
+
+            var controller = new StylistController(test_logger);
+            Stylist s = new Stylist
+            {
+                ID = 1,
+                Name = "test",
+                Level = 1,
+                Bio = "I'm a test"
+            };
+
+            controller.Post(s); //but how can I intercept the SQLiteDbUtility.AddStylist call in the Post function so an insert isn't actually performed?
+        }
+
+
+    }
+}
