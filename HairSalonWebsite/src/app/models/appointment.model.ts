e17b30a03a9231688ebf3d6312aae67288c61524
zeenal <zeenal.prajapati@bobcats.gcsu.edu>
2022-03-02 12:50:53 -0500

    models frontend

---
+++ HairSalonWebsite/src/app/models/appointment.model.ts
@@ -0,0 +1,13 @@
+export class Appointment 
+{
+
+    id?: number;
+    stylistid: number;
+    name: string;
+    email: string;
+    phone: string;
+    date: Date;
+    datecreated: Date;
+    description: string;
+
+}
