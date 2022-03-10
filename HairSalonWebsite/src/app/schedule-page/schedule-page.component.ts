b945f72b874888fc5946bf3b50d94aa56cbe8a39
Jake Morris <jakelm321@gmail.com>
2022-03-10 17:31:09 -0500

    Updated functionality to allow for multiple stylists to be selected

--- HairSalonWebsite/src/app/schedule-page/schedule-page.component.ts
+++ HairSalonWebsite/src/app/schedule-page/schedule-page.component.ts
@@ -30,11 +30,10 @@ export class SchedulePageComponent implements OnInit
   unavailabilitiesList: Unavailability[]
   appointmentList: Appointment[];
   stylistList: Stylist[];
+  selectedSylistList: Stylist[] = []; //list of all stylist selected by user
   
   eventsToShow: CalendarEvent[] = []; //calendar events list of unavailabilities and appointments
-  allEvents: CalendarEvent[]= [];
-  oneOrMoreStylistEvents: CalendarEvent[]= [];
-
+  allEvents: CalendarEvent[]= []; //all calendar events for the schedule page
 
   scheduleLoading: boolean = true; //boolean to load page
 
@@ -96,21 +95,54 @@ export class SchedulePageComponent implements OnInit
   }
 
   /**
-   * Function to show only calendar events of stylists selected by the user. 
+   * Function to maintain all stylists selected on the front end drop down menu and call the showScheduleBy
+   * function to update the calendar. The function checks to see if the stylist passed as a parameter is currently
+   * on the list. If they are not on the list, this indicates that the user selected them for viewing and they are
+   * appended to the list. If they are currently on the list, then that idicates that the user deselected them, and 
+   * they are removed from the list.
+   * @param stylist : stylist to be added or subtracted from the list of stylist selected
+   */
+  changeStylistSelected(stylist: Stylist)
+  {
+    let index = this.selectedSylistList.indexOf(stylist); //get index of stylist on selected list of stylists
+    if(index != -1) //if they are already on list, remove
+    {
+      this.selectedSylistList.splice(index,1);
+    }
+    else //else, add the stylist to the list
+    {
+      this.selectedSylistList.push(stylist);
+    }
+    console.log(this.selectedSylistList);
+    this.showScheduleBy(); //update the events being shown on the calendar
+  }
+
+  /**
+   * Function to show only calendar events of stylists selected by the user. Defaults to 
+   * showing all stylists if no stylists are selected
    * @param stylist : the stylist to update the calendar events list by
    */
-  showScheduleBy(stylist: Stylist)
+  showScheduleBy()
   {
-    this.oneOrMoreStylistEvents = []; //create local list of events for one stylist
+    //check stylist selected to see if it is empty or not
+    if(this.selectedSylistList.length == 0) //if list is empty, show all events
+    {
+      this.eventsToShow = this.allEvents;
+    }
+    else //show all events of selected stylists
+    {
+      this.eventsToShow = []; //create local list of events for one stylist
 
-
-    //get unavaiblites and appointments for the requested stylist
-    for(let appointment of this.appointmentList) //check for appointments
+      //iterate through selectedStylistsList to display all events associated with them
+      for(let stylist of this.selectedSylistList)
+      {
+        //get unavaiblites and appointments for the requested stylist
+        for(let appointment of this.appointmentList) //check for appointments
         {
           if(appointment.stylistID == stylist.id) //load appoinment if stylistID is equal to requested stylist
           {
             //create appointment and push it onto the event list
-            this.oneOrMoreStylistEvents.push(
+            this.eventsToShow.push(
               {
                 start: new Date(appointment.date),
                 title: appointment.name + " - " + appointment.description
@@ -118,23 +150,22 @@ export class SchedulePageComponent implements OnInit
             );
           }  
         }
-    for(let unavailability of this.unavailabilitiesList) //check for unavailabilities
-    {
-      if(unavailability.stylistID == stylist.id) //load unavailability if stylistID is equal to requested stylist
-      {
-        //create unavailability and push it onto the event list
-        this.oneOrMoreStylistEvents.push(
+        for(let unavailability of this.unavailabilitiesList) //check for unavailabilities
+        {
+          if(unavailability.stylistID == stylist.id) //load unavailability if stylistID is equal to requested stylist
           {
-            start: new Date(unavailability.startDate),
-            end: new Date(unavailability.endDate),
-            title: unavailability.stylistName + " time off"
+            //create unavailability and push it onto the event list
+            this.eventsToShow.push(
+              {
+                start: new Date(unavailability.startDate),
+                end: new Date(unavailability.endDate),
+                title: unavailability.stylistName + " time off"
+              }
+            );
           }
-        );
+        }
       }
+      console.log(this.eventsToShow);
     }
-    this.eventsToShow = this.oneOrMoreStylistEvents;
-    console.log(this.eventsToShow);
-    console.log("showing one stylist");
-
   }
 }
