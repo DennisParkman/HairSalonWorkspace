425151bc8992ebf203d77e5560934e71d262d610
zeenal <zeenal.prajapati@bobcats.gcsu.edu>
2022-03-29 18:38:17 -0400

    User Controller and Service

---
+++ HairSalonBackEnd/HairSalonBackEnd/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using HairSalonBackEnd.Database;
+using HairSalonBackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HairSalonBackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        /// something to do with error logging
+        /// </summary>
+        private readonly ILogger<UserController> _logger;
+
+        /// <summary>
+        /// constructor that sets the logger
+        /// </summary>
+        /// <param name="logger">the ILogger to set the logger to</param>
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Adds user to the SQLite Database
+        /// </summary>
+        /// <param name="user">the user to add</param>
+        /// <returns>
+        /// an action result containing the added user (with the database-assigned id) 
+        /// or a BadRequest if there is a failure
+        /// </returns>
+        [HttpPost]
+        public ActionResult<Task<User>> Post([FromBody] User user)
+        {
+            try
+            {
+                User newUser = SQLiteDbUtility.AddUser(user);
+                return Ok(newUser);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Could not add User: " + e.Message);
+            };
+        }
+
+        /// <summary>
+        /// Returns all users found in the SQLite Database
+        /// </summary>
+        /// <returns> all users found in the SQLite Database as an Enurable Array</returns>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return SQLiteDbUtility.GetAllUsers();
+        }
+
+        /// <summary>
+        /// Deletes an user using id in the SQLite Database
+        /// </summary>
+        /// <param name="id">the id of the user to delete</param>
+        [HttpDelete]
+        [Route("{id}")]
+        public void Delete(int id)
+        {
+            SQLiteDbUtility.DeleteUser(id);
+        }
+
+        /// <summary>
+        /// Updates an user in the SQLite Database
+        /// </summary>
+        /// <param name="user">the user to update</param>
+        [HttpPut]
+        public void Put([FromBody] User user)
+        {
+            SQLiteDbUtility.UpdateUser(user);
+        }
+
+    }
+
+}
