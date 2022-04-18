24f6a40f0c704cf6e06443e24dd8ba0c77054a5d
zeenal <zeenal.prajapati@bobcats.gcsu.edu>
2022-04-18 18:07:22 -0400

    read the description for commit message...heeheehee
    You won't find "UnavailabilityService > should be created" in SpecList Failures (Karma) 
    PS:- ng test passes - yoooooo

--- HairSalonWebsite/src/app/services/unavailability-service/unavailability.service.spec.ts
+++ HairSalonWebsite/src/app/services/unavailability-service/unavailability.service.spec.ts
@@ -1,16 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UnavailabilityService } from './unavailability.service';
 
 describe('UnavailabilityService', () => {
-  let service: UnavailabilityService;
+  //let service: UnavailabilityService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UnavailabilityService);
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UnavailabilityService]
+    }));
 
   it('should be created', () => {
+    const service: UnavailabilityService = TestBed.get(UnavailabilityService);
     expect(service).toBeTruthy();
   });
+
 });
