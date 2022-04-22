55b13392ca5b6d955645b631da662b4ecbee36ea
Dennis <dennis.parkman@bobcats.gcsu.edu>
2022-04-22 00:18:54 -0400

    Test

--- HairSalonWebsite/src/app/services/stylist-schedule-service/stylist-schedule.service.spec.ts
+++ HairSalonWebsite/src/app/services/stylist-schedule-service/stylist-schedule.service.spec.ts
@@ -4,9 +4,10 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CalendarEvent } from 'angular-calendar';
 import { CalendarEventActionsComponent } from 'angular-calendar/modules/common/calendar-event-actions.component';
+import { isMonday } from 'date-fns';
 import { Stylist } from 'src/app/models/stylist.model';
-import { StylistHours } from 'src/app/models/stylisthours.model';
-import { Unavailability } from 'src/app/models/unavailability.model';
+import { StylistHours, WeekDay } from 'src/app/models/stylisthours.model';
+import { TimePeriod, Unavailability } from 'src/app/models/unavailability.model';
 
 import { StylistScheduleService } from './stylist-schedule.service';
 
@@ -33,46 +34,144 @@ describe('StylistScheduleService', () => {
   });
 
   it('returned Observable should match the right data', () => {
+    
+    const stylistId = 1;
+
     const mockStylists : Stylist[] = [
         {
             name: "TestStylist",
             level: 5,
             bio: "TestBio",
-            id: 1
+            id: stylistId
         }
     ];
+
+    let testDate = new Date();
+    testDate.setHours(0);
+    testDate.setMinutes(0);
+    testDate.setSeconds(0);
+    testDate.setMilliseconds(0);
+    while(testDate.getDay() != 0)
+    {
+        testDate = new Date(testDate.getTime() + (24 * 60 * 60 * 1000));
+    }
+    // 'testDate' is now the closest future Sunday at midnight
+
     const mockHours : StylistHours[] = [
         { // Overwrite first half of day
-            
+            stylistID: stylistId,
+            day: WeekDay.Monday,
+            startTime: '10:00',
+            endTime: '12:00'
         },
         { // Overwrite last half of day
-
+            stylistID: stylistId,
+            day: WeekDay.Tuesday,
+            startTime: '10:00',
+            endTime: '12:00'
         },
         { // Overwrite full day
-
+            stylistID: stylistId,
+            day: WeekDay.Wednesday,
+            startTime: '10:00',
+            endTime: '12:00'
         },
         { // Overwrite middle of day
-
+            stylistID: stylistId,
+            day: WeekDay.Thursday,
+            startTime: '10:00',
+            endTime: '12:00'
         }
     ];
+    let today = new Date();
+    today.setHours(0);
     const mockUnavailabilities : Unavailability[] = [
+        { // Wipe out all previous hours
+            stylistID: stylistId,
+            startDate: today,
+            endDate: new Date(testDate.getTime() + 1 * (24 * 60 * 60 * 1000)),
+            period: TimePeriod.Once
+        },
         { // Overwrite first half of day
-
+            stylistID: stylistId,
+            startDate: new Date(testDate.getTime() + 1 * (24 * 60 * 60 * 1000) + 9 * (60 * 60 * 1000)),
+            endDate: new Date(testDate.getTime() + 1 * (24 * 60 * 60 * 1000) + 11 * (60 * 60 * 1000)),
+            period: TimePeriod.Once
         },
         { // Overwrite last half of day
-
+            stylistID: stylistId,
+            startDate: new Date(testDate.getTime() + 2 * (24 * 60 * 60 * 1000) + 11 * (60 * 60 * 1000)),
+            endDate: new Date(testDate.getTime() + 2 * (24 * 60 * 60 * 1000) + 13 * (60 * 60 * 1000)),
+            period: TimePeriod.Once
         },
         { // Overwrite full day
-
+            stylistID: stylistId,
+            startDate: new Date(testDate.getTime() + 3 * (24 * 60 * 60 * 1000) + 9 * (60 * 60 * 1000)),
+            endDate: new Date(testDate.getTime() + 3 * (24 * 60 * 60 * 1000) + 13 * (60 * 60 * 1000)),
+            period: TimePeriod.Once
         },
         { // Overwrite middle of day
-
+            stylistID: stylistId,
+            startDate: new Date(testDate.getTime() + 4 * (24 * 60 * 60 * 1000) + 10.5 * (60 * 60 * 1000)),
+            endDate: new Date(testDate.getTime() + 4 * (24 * 60 * 60 * 1000) + 11.5 * (60 * 60 * 1000)),
+            period: TimePeriod.Once
+        },
+        { // Wipe out all extra hours
+            stylistID: stylistId,
+            startDate: new Date(testDate.getTime() + 5 * (24 * 60 * 60 * 1000)),
+            endDate: new Date(testDate.getTime() + (400 * 24 * 60 * 60 * 1000)),
+            period: TimePeriod.Once
         }
     ];
 
     service.getStylistSchedule()
-      .subscribe(courseData => {
-        expect(courseData).toBeNull();
+      .subscribe(stylistHours => {
+        expect(stylistHours).toBeTruthy();
+        let currentStylistHours = stylistHours[stylistId];
+        expect(currentStylistHours).toBeTruthy();
+        // Thursday should be deleted
+        expect(currentStylistHours.find(x => { x.start.getDay() == 3 })).toBeUndefined();
+
+        let mondaySchedule = currentStylistHours[0];
+        let tuesdaySchedule = currentStylistHours[1];
+        let thursdayFirstSchedule = currentStylistHours[2];
+        let thursdaySecondSchedule = currentStylistHours[3];
+        
+        expect(mondaySchedule?.start.getHours()).toBe(11);
+        expect(mondaySchedule?.end).toBeTruthy();
+        if(mondaySchedule?.end == null)
+        {
+            return;
+        }
+        expect(mondaySchedule?.end.getHours()).toBe(12);
+        
+        expect(tuesdaySchedule?.start.getHours()).toBe(10);
+        expect(tuesdaySchedule?.end).toBeTruthy();
+        if(tuesdaySchedule?.end == null)
+        {
+            return;
+        }
+        expect(tuesdaySchedule?.end.getHours()).toBe(11);
+
+        // Wednesday is deleted
+        
+        expect(thursdayFirstSchedule?.start.getHours()).toBe(10);
+        expect(thursdayFirstSchedule?.end).toBeTruthy();
+        if(thursdayFirstSchedule?.end == null)
+        {
+            return;
+        }
+        expect(thursdayFirstSchedule?.end.getHours()).toBe(10);
+        expect(thursdayFirstSchedule?.end.getMinutes()).toBe(30);
+
+        expect(thursdaySecondSchedule?.start.getHours()).toBe(11);
+        expect(thursdaySecondSchedule?.start.getMinutes()).toBe(30);
+        expect(thursdaySecondSchedule?.end).toBeTruthy();
+        if(thursdaySecondSchedule?.end == null)
+        {
+            return;
+        }
+        expect(thursdaySecondSchedule?.end.getHours()).toBe(12);
       });
 
     const req1 = TestBed.inject(HttpTestingController).expectOne('http://localhost:63235/Unavailability');
